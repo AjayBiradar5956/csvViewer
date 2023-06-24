494aa38656ca41ca02df91cb1c248c58e6d30819
AjayBiradar5956 <ajay99biradar@gmail.com>
2023-06-24 22:36:11 +0530

    update

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 
 //Connect to your own db
-const db = "mongodb+srv://ajay99biradar:<password>@cluster0.vkzditd.mongodb.net/?retryWrites=true&w=majority";
+const db = "mongodb+srv://ajay99biradar:ajay362AJAY@cluster0.vkzditd.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose.connect(db).then(() => {
     console.log("DB connected");
